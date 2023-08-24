e98a6b5b30079fdae0f265768d11caec58b8441b
Jo Rabin <jo@linguafranca.org>
2023-08-24 19:12:24 +0100

    modelling data at sharer and data at helper

--- src/main/java/com/thebuildingblocks/derec/model/Helper.java
+++ src/main/java/com/thebuildingblocks/derec/model/Helper.java
@@ -6,6 +6,9 @@ import java.security.PublicKey;
 import java.security.cert.X509Certificate;
 import java.util.List;
 
+/**
+ * Information a helper knows about itself
+ */
 public class Helper {
     Util.DeRecId id; // helper's Id
     KeyPair keyPair; // public/private key pair
@@ -14,6 +17,10 @@ public class Helper {
     List<String> availableVersions; // a list of available protocol versions
     Util.RetryParameters retryParameters; // parameters to negotiate for any sharer/secret
     List<Sharer> sharer; // list of paired sharers
+
+    /**
+     * Information a helper know about a sharer
+     */
     public static class Sharer {
         Util.DeRecId sharerId; // sharer unique id
         URI sharerUri; // how to contact sharer
@@ -24,8 +31,11 @@ public class Helper {
         List<Share> shares; // the kept shares
     }
 
+    /**
+     * Information a helper knows about a share
+     */
     public static class Share {
-        byte [] shareContent; // contents of the shre
+        byte [] shareContent; // contents of the share
         int shareVersion; // the version of the share
         byte[] signature; // the signature attached to the share
     }
