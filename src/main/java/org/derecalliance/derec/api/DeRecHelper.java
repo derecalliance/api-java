56980c11c792f77ebd02adcff08cb3b80bb11426
Jo <jo@linguafranca.org>
2023-11-08 16:14:12 +0000

    Improve helper definition

--- src/main/java/org/derecalliance/derec/api/DeRecHelper.java
+++ src/main/java/org/derecalliance/derec/api/DeRecHelper.java
@@ -18,7 +18,8 @@
 package org.derecalliance.derec.api;
 
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.Function;
+
 /**
  * A DeRec Helper implementation may implement this API to provide access to and visibility of its operation. The
  * implementation may provide non-standardised means of automatically accepting pairing requests and is assumed to
@@ -40,10 +41,18 @@ public interface DeRecHelper {
 
 		PairingResponseStatus getStatus();
 
+		/**
+		 * How the pairing request was responded to
+		 */
 		enum PairingResponseStatus {
-			PAIRED, // replied positively
-			REFUSED, // replied negatively
-			REMOVED, // at sharer request, potentially as a result of a DISCONNECT from helper
+			/*
+			This is needed only if we as a standard wish to report refused and gone
+			sharers to the application. As things stand this interface only reports
+			active secrets.
+			REPLIED_PAIRED, // replied positively
+			REPLIED_REFUSED, // replied negatively*/
+			ACTIVE,
+			REPLIED_REMOVED, // at sharer request, potentially as a result of a DISCONNECT from helper
 			PENDING_DISCONNECTION // disconnecting at Helper Request
 		}
 	}
@@ -59,8 +68,31 @@ public interface DeRecHelper {
 	}
 
 	interface Notification {
+		Type getType(); // the type of the notification
+		DeRecIdentity getSharerId(); // the sharer id
+		DeRecSecret.Id getSecretId(); // the secretId or null if none
+		int getVersion(); // the version number or -1 if inapplicable
 
+
+		interface Type {
+			String name();
+		}
+
+		enum StandardHelperNotificationType implements Type {
+			PAIR_INDICATION, // someone is trying to pair for s particular secret
+			UNPAIR_INDICATION, // someone is unpairing for a particular secret
+			UPDATE_INDICATION, // an update has been received for a secret
+			VERIFY_INDICATION, // a verification request has been received for a secret
+			LIST_SECRETS_INDICATION, // a request to list secrets has been received
+			RECOVER_SECRET_INDICATION // a request to recover a secret has been received
+		}
 	}
+
+	interface NotificationResponse {
+		boolean getDisconnectPlease(); // set to true if the helper wishes to refuse the request and discontinue the helper relationship
+		boolean getReason(String reason); // some optional explanation (see https://en.wikipedia.org/wiki/Never_complain,_never_explain)
+	}
+
 
 	/**
 	 * Get a list of all protected items known to this helper
@@ -71,15 +103,12 @@ public interface DeRecHelper {
 
 	/**
 	 * Provide a "listener" for status and lifecycle event notifications relating to this helper's information,
-	 * such as changes in the list of shares or requests to pair.
+	 * such as changes in the list of shares or requests to pair. The listener both provides information about
+	 * an event and also allows the listener to respond positively, or negatively with a reason, which
+	 * causes the pairing relationship to end.
 	 * <p>
 	 * Note: More than one listener may be provided by composition such as:
 	 * <p>
-	 * <pre>{@code
-	 * Consumer<DeRecStatusNotification> listener1 = n -> log(n.getType().name());
-	 * Consumer<DeRecStatusNotification> listener2 = n -> {if (n.getSeverity().equals(ERROR)) alert(n.getType().name());};
-	 * sharer.setListener(listener1.andThen(listener2));
-	 * }</pre>
 	 */
-	void setListener(Consumer<DeRecHelper.Notification> listener);
+	void setListener(Function<Notification, NotificationResponse> listener);
 }
