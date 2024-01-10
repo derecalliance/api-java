243c006b73938633f303d2818900966a38c6be80
Dipti Mahamuni <63212804+diptimahamuni@users.noreply.github.com>
2024-01-10 10:37:30 -0800

    Copyright updates (#16)
    Signed-off-by: Dipti Mahamuni <dipti@mahamuni.net>

--- src/main/java/org/derecalliance/derec/api/DeRecSharerStatus.java
+++ src/main/java/org/derecalliance/derec/api/DeRecSharerStatus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Swirlds Labs.
+ * Copyright (c) DeRec Alliance and its Contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
